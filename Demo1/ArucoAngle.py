06e14e3e42733100f0052b5561740daad4b9cf26
bruce <bbearden@mines.edu>
2025-03-08 15:02:32 -0700

    Update angle only when it changes

--- Demo1/ArucoAngle.py
+++ Demo1/ArucoAngle.py
@@ -2,6 +2,12 @@ import numpy as np
 import cv2 as cv
 from time import sleep, time
 import pickle
+import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
+import threading
+import queue
+import board
+
+
 ''' TODO:
 - Add threading to the LCD,
 - make functions
@@ -14,14 +20,56 @@ with open("cameraMatrix.pkl", "rb") as f:
 with open("dist.pkl", "rb") as f:
     dist = pickle.load(f)
 
+# Initialise the queue where the updated marker quadrants are stored
+lcdQueue = queue.Queue()
+
 # Aruco marker dictionary and detector parameters
 dictionary = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_6X6_50) # using a 6x6 marker
 parameters = cv.aruco.DetectorParameters()
+
+
+# Initialise LCD rows and cols
+i2cLCD = board.I2C()  # uses board.SCL and board.SDA
+
+LCD_COLUMS = 16
+LCD_ROWS = 2
+lcd = character_lcd.Character_LCD_RGB_I2C(i2cLCD, LCD_COLUMS, LCD_ROWS)
 
 # init the camera
 cap = cv.VideoCapture(0) 
 currentAngle = None
 lastAngle = None
+
+
+def updateLCD():
+    print("Started LCD Thread")
+
+    #*********
+    # LCD Init
+    #*********
+    
+    # Initialise the LCD 
+    lcd = character_lcd.Character_LCD_RGB_I2C(i2cLCD, LCD_COLUMS, LCD_ROWS)
+    lcd.clear()
+    lcd.color = [100, 0, 0]  # Set initial color (red)
+    print("LCD init done")
+
+    #***********************************************
+    # Update the LCD and send data only if necessary
+    #***********************************************
+    while True:
+        if not lcdQueue.empty():
+            angle = lcdQueue.get()
+            lcd.clear()
+            
+##            if update:
+            lastAngle = angle # temp assignment in case of quick change in marker position
+            lcd.message = str(angle)
+            # Send the wheelLocation data to the Arduino using smbus2
+
+# Start the LCD update thread
+myThread = threading.Thread(target=updateLCD, daemon=True)
+myThread.start()
 
 # repeat until user hits 'q'
 while True: 
@@ -56,15 +104,17 @@ while True:
             cv.drawFrameAxes(gray, cameraMatrix, dist, rvec, tvec, 0.03)
             
             # Print angle only when changed by 2 decimal points
-            if currentAngle != lastAngle:
+            if currAngle != lastAngle:
                 # TODO add to the queue
-                print(f"Marker ID {ids[i][0]}: Angle = {currAngle} degrees")
+                lcdQueue.put(currentAngle)  # Add the new angle to the queue
+
+##                print(f"Marker ID {ids[i][0]}: Angle = {currAngle} degrees")
                 lastAngle = currAngle
     else:
         currAngle = None
 
     # Show the frame
-    cv.imshow("Aruco Detection", frame)
+    cv.imshow("Aruco Detection", gray)
     
     # Exit on key press
     if cv.waitKey(1) & 0xFF == ord('q'):
@@ -72,3 +122,4 @@ while True:
 
 cap.release()
 cv.destroyAllWindows()
+print("Done")
