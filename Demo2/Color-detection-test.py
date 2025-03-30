9857bbd3c515e3a41de772098a316aa44eb8d26f
bbearden07 <brucebearden5150@gmail.com>
2025-03-30 12:23:40 -0600

    Color

--- Demo2/Color-detection-test.py
+++ Demo2/Color-detection-test.py
@@ -37,36 +37,42 @@ while True:
             # Draw the detected marker on the frame
             cv.aruco.drawDetectedMarkers(frame, corners, ids)
 
-            # Define the region to the right of the marker (with an offset)
-            offset = 20  # Pixels to shift the region to the right
-            roi_x_min = max(0, x_max + offset)
-            roi_x_max = min(frame.shape[1], roi_x_min + (x_max - x_min))
+            # Define the region to the right and left of the marker (with an offset)
+            offset = 30  # Pixels to shift the region to the right or left
+            roi_x_min_left = max(0, x_min - offset)
+            roi_x_max_left = x_min
+            roi_x_min_right = x_max
+            roi_x_max_right = min(frame.shape[1], x_max + offset)
             roi_y_min = y_min
             roi_y_max = y_max
 
-            # Extract the region of interest (ROI) to the right of the marker
-            roi = frame[roi_y_min:roi_y_max, roi_x_min:roi_x_max]
+            # Extract the region of interest (ROI) for the right side of the marker (red arrow)
+            roi_right = frame[roi_y_min:roi_y_max, roi_x_min_right:roi_x_max_right]
 
-            # Convert the ROI to HSV color space
-            hsv_roi = cv.cvtColor(roi, cv.COLOR_BGR2HSV)
+            # Extract the region of interest (ROI) for the left side of the marker (green arrow)
+            roi_left = frame[roi_y_min:roi_y_max, roi_x_min_left:roi_x_max_left]
+
+            # Convert both ROIs to HSV color space
+            hsv_right = cv.cvtColor(roi_right, cv.COLOR_BGR2HSV)
+            hsv_left = cv.cvtColor(roi_left, cv.COLOR_BGR2HSV)
 
             # Define color ranges for red and green in HSV
             red_range_1 = [(0, 120, 70), (10, 255, 255)]  # Lower red range
             red_range_2 = [(170, 120, 70), (180, 255, 255)]  # Upper red range
             green_range = [(40, 40, 40), (80, 255, 255)]  # Green range
 
-            # Threshold the HSV image to detect red and green colors
-            red_mask_1 = cv.inRange(hsv_roi, np.array(red_range_1[0]), np.array(red_range_1[1]))
-            red_mask_2 = cv.inRange(hsv_roi, np.array(red_range_2[0]), np.array(red_range_2[1]))
-            red_mask = cv.bitwise_or(red_mask_1, red_mask_2)  # Combine both red ranges
+            # Threshold the HSV images to detect red and green colors
+            red_mask_right_1 = cv.inRange(hsv_right, np.array(red_range_1[0]), np.array(red_range_1[1]))
+            red_mask_right_2 = cv.inRange(hsv_right, np.array(red_range_2[0]), np.array(red_range_2[1]))
+            red_mask_right = cv.bitwise_or(red_mask_right_1, red_mask_right_2)  # Combine both red ranges
 
-            green_mask = cv.inRange(hsv_roi, np.array(green_range[0]), np.array(green_range[1]))
+            green_mask_left = cv.inRange(hsv_left, np.array(green_range[0]), np.array(green_range[1]))
 
-            # Check if any pixels are detected for red or green color
-            if np.count_nonzero(red_mask) > 0:
-                color_detected = "Red color detected!"
-            elif np.count_nonzero(green_mask) > 0:
-                color_detected = "Green color detected!"
+            # Check if any pixels are detected for red or green color in the right or left regions
+            if np.count_nonzero(red_mask_right) > 0:
+                color_detected = "Red arrow on the right!"
+            elif np.count_nonzero(green_mask_left) > 0:
+                color_detected = "Green arrow on the left!"
 
     # Display the detected color on the frame
     cv.putText(frame, color_detected, (10, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv.LINE_AA)
