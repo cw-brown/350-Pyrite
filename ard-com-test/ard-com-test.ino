93fdfa7b89ba0bd4004dec550d3d23b71cccc9b7
bbearden07 <129981162+bbearden07@users.noreply.github.com>
2025-04-03 08:40:19 -0600

    Update ard-com-test.ino

--- ard-com-test/ard-com-test.ino
+++ ard-com-test/ard-com-test.ino
@@ -1,36 +1,37 @@
 #include <Wire.h>
 
-#define I2C_ADDR 0x08  // I2C address of the Arduino
+#define I2C_ADDR 0x08
 
-union FloatPair {
-    float values[2];  // Two floats stored together
-    byte bytes[8];    // Same memory as bytes
+union FloatUnion {
+    float value;
+    byte bytes[4];
 };
 
-FloatPair receivedData;
+FloatUnion f1, f2;
+byte receivedData[8];
+int index = 0;
 
 void receiveData(int byteCount) {
-    if (byteCount == 8) {  // Ensure we receive exactly 8 bytes (2 floats)
-        for (int i = 0; i < 8; i++) {
-            receivedData.bytes[i] = Wire.read();  // Read bytes into union
+    while (Wire.available()) {
+        receivedData[index++] = Wire.read();
+        if (index >= 8) {
+            index = 0;
+            memcpy(f1.bytes, receivedData, 4);
+            memcpy(f2.bytes, receivedData + 4, 4);
+            Serial.print("Received floats: ");
+            Serial.print(f1.value);
+            Serial.print(", ");
+            Serial.println(f2.value);
         }
-
-        // Print the received floats
-        Serial.print("Received floats: ");
-        Serial.print(receivedData.values[0]);
-        Serial.print(", ");
-        Serial.println(receivedData.values[1]);
-    } else {
-        Serial.println("Error: Incomplete data received");
     }
 }
 
 void setup() {
-    Serial.begin(115200);
-    Wire.begin(I2C_ADDR);  // Initialize I2C with the address
-    Wire.onReceive(receiveData);  // Set the function to be called on data receipt
+    Serial.begin(9600);
+    Wire.begin(I2C_ADDR);
+    Wire.onReceive(receiveData);
 }
 
 void loop() {
-    // Nothing needed in the loop for now
+    delay(100);
 }
