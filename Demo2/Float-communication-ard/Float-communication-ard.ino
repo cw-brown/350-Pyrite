bc02bda8c642f8f6ab8d543db20558223cb089e4
bbearden07 <brucebearden5150@gmail.com>
2025-04-04 17:29:21 -0600

    Added new ard com test

--- Demo2/Float-communication-ard/Float-communication-ard.ino
+++ Demo2/Float-communication-ard/Float-communication-ard.ino
@@ -1,43 +1,42 @@
 #include <Wire.h>
 
 const int slaveAddress = 8;  // I2C address of the slave device
-float vector[2];  // 2-element vector to store received floats
-byte extraByte;   // Extra byte (9th byte) that we will ignore
+float vector[2];             // Distance and angle
+uint8_t flag1, flag2;        // Two flag bytes
 
 void setup() {
   Serial.begin(115200);
-  Wire.begin(slaveAddress);  // Start I2C as slave with the given address
-  Wire.onReceive(receiveEvent);  // Function to call when master sends data
+  Wire.begin(slaveAddress);          // Start I2C as slave
+  Wire.onReceive(receiveEvent);      // Register callback
   Serial.println("Arduino is ready to receive data...");
 }
 
 void loop() {
-  // Nothing needed here, as we handle the data in the receive event
+  // Nothing here; event-driven receive
 }
 
-// Function to receive data from the master
 void receiveEvent(int bytes) {
-//  Serial.print("Bytes received: ");
-//  Serial.println(bytes);
+  if (bytes >= 10) {
+    // Read flags
+    flag1 = Wire.read();  // First byte
+    flag2 = Wire.read();  // Second byte
 
-  // Ensure we have at least 9 bytes, where we ignore the first byte
-  if (bytes >= 9) {
-    // Read and ignore the first byte (offset byte)
-    Wire.read();  // Discard the first byte
-
-    // Read 8 bytes into the vector (first 2 floats)
+    // Read 8 bytes into float array (2 floats)
     byte* data = (byte*) &vector;
     for (int i = 0; i < sizeof(vector); i++) {
-      data[i] = Wire.read();  // Store bytes into vector
+      data[i] = Wire.read();
     }
 
-    // Print the received vector values
-//    Serial.print("Received vector: ");
-    Serial.print(vector[0], 2);  // Print first float with 2 decimal places
+    // Print everything
+    Serial.print("Flags: ");
+    Serial.print(flag1);
     Serial.print(", ");
-    Serial.println(vector[1], 2);  // Print second float with 2 decimal places
+    Serial.print(flag2);
+    Serial.print(" | Vector: ");
+    Serial.print(vector[0], 2);  // Distance
+    Serial.print(", ");
+    Serial.println(vector[1], 2);  // Angle
   } else {
-    // If less than 9 bytes, report an error
-    Serial.println("Error: Unexpected number of bytes received.");
+    Serial.println("Error: Not enough bytes received.");
   }
 }
