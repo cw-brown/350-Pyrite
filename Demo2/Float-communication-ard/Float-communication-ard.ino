7981d5faaaf473790dab9051720dc115a1e55787
bbearden07 <brucebearden5150@gmail.com>
2025-04-04 18:00:23 -0600

    Hello

--- Demo2/Float-communication-ard/Float-communication-ard.ino
+++ Demo2/Float-communication-ard/Float-communication-ard.ino
@@ -1,46 +1,42 @@
 #include <Wire.h>
 
 const int slaveAddress = 8;
-float vector[2] = {0.0, 0.0};
-uint8_t flag1 = 0, flag2 = 0;
-bool newData = false;
+float vector[2];      // distance, angle
+uint8_t flag1, flag2; // 2 flags
 
 void setup() {
   Serial.begin(115200);
   Wire.begin(slaveAddress);
   Wire.onReceive(receiveEvent);
-  Serial.println("Arduino is ready to receive data...");
+  Serial.println("Arduino ready to receive data...");
 }
 
 void loop() {
-  if (newData) {
-    Serial.print("Flags: ");
-    Serial.print(flag1);
-    Serial.print(", ");
-    Serial.print(flag2);
-    Serial.print(" | Vector: ");
-    Serial.print(vector[0], 2);  // Distance
-    Serial.print(", ");
-    Serial.println(vector[1], 2);  // Angle
-
-    newData = false;  // Reset flag after printing
-  }
-
-  delay(100);  // Slight delay to avoid spamming the serial monitor
+  // Nothing here – all done in receiveEvent
 }
 
 void receiveEvent(int bytes) {
   if (bytes >= 10) {
+    // Read flags
     flag1 = Wire.read();
     flag2 = Wire.read();
 
-    byte* data = (byte*) &vector;
+    // Read 8 bytes into float vector
+    byte* data = (byte*)&vector;
     for (int i = 0; i < sizeof(vector); i++) {
       data[i] = Wire.read();
     }
 
-    newData = true;  // Mark new data received
+    // Print all values
+    Serial.print("Flags: ");
+    Serial.print(flag1);
+    Serial.print(", ");
+    Serial.print(flag2);
+    Serial.print(" | Distance: ");
+    Serial.print(vector[0], 2);
+    Serial.print(" ft, Angle: ");
+    Serial.println(vector[1], 2);
   } else {
-    Serial.println("Error: Not enough bytes received.");
+    Serial.println("ERROR: Received < 10 bytes!");
   }
 }
