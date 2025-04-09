9704ff42a0acf4b8b7ddb5d42e89b836b6745d77
bbearden07 <brucebearden5150@gmail.com>
2025-04-09 10:30:15 -0600

    Added Header to Demo2.py

--- Demo2/Demo2.py
+++ Demo2/Demo2.py
@@ -11,6 +11,35 @@ import struct
 
 
 ''' 
+This Python code enables a robot to autonomously detect a visual beacon using an Aruco marker 
+ID, navigate to within 1.5 feet of it, and perform a directional turn based on the color of the
+arrow indicator. 
+
+The code supports two types of trials but edits to the DO_TURN variable in the Arduino Code are necessary: 
+(1) approach and stop within 1.5 feet of the marker, 
+and (2) approach and turn 90° in the correct direction. 
+
+Performance is evaluated based on accuracy, timing, and reliability across runs.
+
+The I2C communication protocol is used to send a string to the arduino to display the
+desired location of the left wheel and right wheel, and to send a byte to the
+arduino to commincate the desired location.
+
+To run this code, ensure that the aruco marker used for testing is defined in the
+imported aruco dictionary, and verify the following connections from the lcd pi shield
+and the arduino:
+
+    *NOTE: The LCD pi shield needs to properly attached where the left side of the shield
+           is lined up with the SD card side of the PI. View the EENG 350 Assingment 1
+           computer vision and communication tutorial for more details.
+
+    Connections:
+    pi        -        arduino
+    --------------------------
+    GND       -        GND
+    SCL       -        A5
+    SDA       -        A4
+
 '''
 # define the I2C Address of Arduino
 ARD_ADDR = 8  
@@ -21,30 +50,15 @@ i2cARD = smbus2.SMBus(1)  # Use I2C bus 1 for communication with Arduino
 # init the camera
 cap = cv.VideoCapture(0)
 
-### Initialise the queue where a distance and vector is stored
-##lcdQueue = queue.Queue()
-
 # Camera Calibration and Distortion Matricies
 with open("cameraMatrix.pkl", "rb") as f:
     cameraMatrix = pickle.load(f)
 with open("dist.pkl", "rb") as f:
     dist = pickle.load(f)
 
-
-
 # Aruco marker dictionary and detector parameters
 dictionary = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_6X6_50) # using a 6x6 marker
 parameters = cv.aruco.DetectorParameters()
-
-
-### Initialise LCD rows and cols COMMENT IN FOR LCD
-##i2cLCD = board.I2C()  # uses board.SCL and board.SDA
-
-##LCD_COLUMS = 16
-##LCD_ROWS = 2
-##lcd = character_lcd.Character_LCD_RGB_I2C(i2cLCD, LCD_COLUMS, LCD_ROWS)
-
-
 
 # Angle, distance and color place holders
 currAngle = None
@@ -57,31 +71,6 @@ lcdPrompt = []
 
 # Implement time based sending to minimize any lag
 last_update_time = time()
-
-# LCD thread to minimize delay in the system
-##def updateLCD():
-####    print("Started LCD Thread")
-##    # LCD Init
-##    lcd = character_lcd.Character_LCD_RGB_I2C(i2cLCD, LCD_COLUMS, LCD_ROWS)
-##    lcd.clear()
-##    lcd.color = [10, 0, 0]  # Set initial color (red)
-####    print("LCD init done")
-##
-##    # Update the LCD only when necessary
-##    while True:
-##        if not lcdQueue.empty():
-##            message = lcdQueue.get()
-####        else:
-####            message = [9.9,99.9]
-####        i2cARD.write_i2c_block_data(ARD_ADDR, 0x00, message)
-##
-##        #*************************************************
-##        # To send a string
-##            message = str(message)
-##            lcd.clear() 
-##            lcd.message = message
-##        # Send the wheelLocation data to the Arduino using smbus2
-
 
 # find the angle off the center axis of rotation. return an angle to 1 decimal point of accuracy      
 def get_angle(cnrs):
@@ -135,7 +124,7 @@ def get_color(cnrs, frame, ids):
         # Convert both ROIs to HSV color space
         hsv_right = cv.cvtColor(roi_right, cv.COLOR_BGR2HSV) if roi_right.size > 0 else None
         hsv_left = cv.cvtColor(roi_left, cv.COLOR_BGR2HSV) if roi_left.size > 0 else None
-##        print(hsv_left)
+
         # Define color ranges for red and green in HSV
         red_range_1 = [(0, 120, 70), (5, 255, 255)]  # Lower red range
         red_range_2 = [(170, 120, 70), (175, 255, 255)]  # Upper red range
@@ -151,22 +140,15 @@ def get_color(cnrs, frame, ids):
         
         if hsv_left is not None:
             green_mask_left = cv.inRange(hsv_left, np.array(green_range[0]), np.array(green_range[1]))
+        
         # Check if any pixels are detected for red or green color in the right or left regions
         if red_mask_right is not None and np.count_nonzero(red_mask_right) > 0:
             color = -90.0 #Red
-##            print("-90.0 / Red")
+
         elif green_mask_left is not None and np.count_nonzero(green_mask_left) > 0:
             color = 90.0 #Green
-##            print("90.0 / Green")
-##    cropped_frame = frame[roi_y_min:roi_y_max, roi_x_min_left:roi_x_max_right]
-##    cv.imshow("Aruco Detection", cropped_frame)
-##    print(color)
 
     return color
-
-# Start the LCD update thread
-##myThread = threading.Thread(target=updateLCD, daemon=True)
-##myThread.start()
 
 # Run until the user types 'q' to quit
 while True:
@@ -174,7 +156,7 @@ while True:
     # Capture the frame
     ret, frame = cap.read()
     if not ret:
-        print("Shit")
+        print("Camera Error")
         continue
     
     # Undistort frame
@@ -195,63 +177,38 @@ while True:
         # Check the distance ( > 1 or <= 1), only detect color within 1 foot, and detect angle outside 1 foot
         if currDistance <= 1.7:  
             currAngle = get_color(corners,frame,ids) # Set the angle to the detected color
-##            currDistance = 1 # tell the robot to not move
         else:
             currAngle = get_angle(corners) # find the angle
             currColor = 0 # unused input, set it to something so no errors
     
         message = [currDistance, currAngle] # compile the current Distance and angle into a message to send to the arduino
-         # Convert the floats into a byte array (4 bytes each)
-##        data = struct.pack('ff', message[0], message[1])
-##        
-##        # Send the packed float data (8 bytes total)
-##        i2cARD.write_i2c_block_data(ARD_ADDR, 0, list(data))
-##        message = str(message) # turn it into a string
-##        command = [ord(character) for character in message]
-##        i2cARD.write_i2c_block_data(ARD_ADDR, 0x00, command[1:-1])
    
-
-        # Update LCD only when necessary (based on the angle, distance, or color change)
         if time() - last_update_time >= 0.5 and (currAngle != lastAngle or currDistance != lastDistance):
-##        if time() - last_update_time >= 1:
             if currAngle != lastAngle:
                 lastAngle = currAngle # update the angle
             if currDistance != lastDistance:
                 lastDistance = currDistance # update the distance
             if currColor != lastColor:
                 lastColor = currColor # update the color
-##            lcdPrompt = [currDistance, currAngle]
-##            lcdQueue.put(lcdPrompt)
-##            last_update_time = time()    
 
     else:
         currAngle = 99.9
         currDistance = 99.9
         if currDistance != lastDistance or currAngle != lastAngle:
             lastAngle = currAngle
-            lastDistance = currDistance
-##            lcdPrompt = [currDistance, currAngle]
-##            lcdQueue.put(lcdPrompt)
-##            last_update_time = time()  
+            lastDistance = currDistance  
             
         currColor = 1 # place holder value
         message = [currDistance, currAngle] # compile the current Distance and angle into a message to send to the arduino
-        
-         # Convert the floats into a byte array (4 bytes each)
-##    data = struct.pack('ff', message[0], message[1])
-        
-        # Send the packed float data (8 bytes total)
+
+    # Try to send data, if I2C error, continue to next iteration    
     try:
-##        print(message[0])
-##        print(message[1])
         data = struct.pack('ff', message[0], message[1])
-        i2cARD.write_i2c_block_data(ARD_ADDR, 0, list(data))
-        # Show the (gray) frame
-        
+        i2cARD.write_i2c_block_data(ARD_ADDR, 0, list(data))   
     except:
         continue
-##    cv.imshow("Aruco Detection", frame)
-    # Quit when the user hits 'q'
+
+    # Quit if user hits q
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
@@ -259,4 +216,4 @@ while True:
 cap.release()
 cv.destroyAllWindows()
 
-print("Done")
+print("Exit")
