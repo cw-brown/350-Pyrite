b418e276a67a23e21b2d5e02ac069ae2f21020db
bbearden07 <brucebearden5150@gmail.com>
2025-04-27 13:14:35 -0600

    Update CropTOp2

--- Final/CropTop2.py
+++ Final/CropTop2.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy as np 
 import cv2 as cv
 from time import sleep, time
 import pickle
@@ -9,10 +9,11 @@ import board
 import smbus2
 import struct
 
-'''
+
+''' 
 This Python code enables a robot to autonomously detect a visual beacon using an Aruco marker 
 ID, navigate to within 1.5 feet of it, and perform a directional turn based on the color of the
-arrow indicator. Only the closest marker is processed for distance, angle, and color detection.
+arrow indicator. 
 
 The code supports two types of trials but edits to the DO_TURN variable in the Arduino Code are necessary: 
 (1) approach and stop within 1.5 feet of the marker, 
@@ -38,6 +39,7 @@ and the arduino:
     GND       -        GND
     SCL       -        A5
     SDA       -        A4
+
 '''
 
 COLOR_THRESH = 1
@@ -65,9 +67,9 @@ parameters = cv.aruco.DetectorParameters()
 currAngle = None
 lastAngle = None
 currDistance = None
-lastDistance = None
+lastDistance= None
 currColor = None
-lastColor = None
+lastColor= None
 lcdPrompt = []
 
 # Implement time based sending to minimize any lag
@@ -76,79 +78,90 @@ last_update_time = time()
 # find the angle off the center axis of rotation. return an angle to 1 decimal point of accuracy      
 def get_angle(cnrs):
     rvecs, tvecs, _ = cv.aruco.estimatePoseSingleMarkers(cnrs, 0.05, cameraMatrix, dist)
-    rvec = rvecs[0][0]
-    tvec = tvecs[0][0]
-    
-    # Compute rotation matrix
-    R, _ = cv.Rodrigues(rvec)
-    
-    # Angle between camera axis and marker
-    angle = -1 * np.degrees(np.arctan2(tvec[0], tvec[2]))
-    angle = float(round(angle, 1)) # round the angle to 1 decimal point
+        
+    for i in range(len(ids)):
+        rvec = rvecs[i][0]
+        tvec = tvecs[i][0]
+        
+        # Compute rotation matrix
+        R, _ = cv.Rodrigues(rvec)
+        
+        # Angle between camera axis and marker
+        angle = -1*np.degrees(np.arctan2(tvec[0], tvec[2]))
+        angle = float(round(angle, 1)) # round the angle to 2 decimal points. 
     return angle
 
-# find the distance to the marker return a distance of 1 decimal point of accuracy
+# find the distance to the marker retrun a distance of 1 decimal point of accuracy
 def get_distance(cnrs):
     rvecs, tvecs, _ = cv.aruco.estimatePoseSingleMarkers(cnrs, 0.05, cameraMatrix, dist)
-    tvec = tvecs[0][0]
-    distance = float(round(tvec[2] * 3.28, 1))  # Convert meters to feet
+    for i in range(len(ids)):
+        rvec = rvecs[i][0]
+        tvec = tvecs[i][0]
+        distance = float(round(tvec[2]*3.28,1))  # Convert meters to feet
     return distance
 
 def get_color(cnrs, frame, ids):
-    color = None
-    # Get the coordinates of the marker corners
-    marker_corners = cnrs[0]
-    x_min, y_min = np.min(marker_corners, axis=0).astype(int)
-    x_max, y_max = np.max(marker_corners, axis=0).astype(int)
+##    print("color")
+    for i in range(len(ids)):
+        color = None
+        # Get the coordinates of the marker corners
+        marker_corners = cnrs[i][0]
+        x_min, y_min = np.min(marker_corners, axis=0).astype(int)
+        x_max, y_max = np.max(marker_corners, axis=0).astype(int)
 
-    # Crop the image
-    offset = 70  # Pixels to shift the region to the right or left
-    roi_x_min_left = max(0, x_min - offset)
-    roi_x_max_left = x_min
-    roi_x_min_right = x_max
-    roi_x_max_right = min(frame.shape[1], x_max + offset)
-    roi_y_min = y_min
-    roi_y_max = y_max
+        # Crop the image
+        offset = 70  # Pixels to shift the region to the right or left
+        roi_x_min_left = max(0, x_min - offset)
+        roi_x_max_left = x_min
+        roi_x_min_right = x_max
+        roi_x_max_right = min(frame.shape[1], x_max + offset)
+        roi_y_min = y_min
+        roi_y_max = y_max
 
-    # Extract the region of interest (ROI) for the right side of the marker (red arrow)
-    roi_right = frame[roi_y_min:roi_y_max, roi_x_min_right:roi_x_max_right]
+        # Extract the region of interest (ROI) for the right side of the marker (red arrow)
+        roi_right = frame[roi_y_min:roi_y_max, roi_x_min_right:roi_x_max_right]
 
-    # Extract the region of interest (ROI) for the left side of the marker (green arrow)
-    roi_left = frame[roi_y_min:roi_y_max, roi_x_min_left:roi_x_max_left]
+        # Extract the region of interest (ROI) for the left side of the marker (green arrow)
+        roi_left = frame[roi_y_min:roi_y_max, roi_x_min_left:roi_x_max_left]
 
-    # Convert both ROIs to HSV color space
-    hsv_right = cv.cvtColor(roi_right, cv.COLOR_BGR2HSV) if roi_right.size > 0 else None
-    hsv_left = cv.cvtColor(roi_left, cv.COLOR_BGR2HSV) if roi_left.size > 0 else None
+        # Convert both ROIs to HSV color space
+        hsv_right = cv.cvtColor(roi_right, cv.COLOR_BGR2HSV) if roi_right.size > 0 else None
+        hsv_left = cv.cvtColor(roi_left, cv.COLOR_BGR2HSV) if roi_left.size > 0 else None
 
-    redLower = np.array([0, 150, 70], dtype=np.uint8)
-    redUpper = np.array([10, 255, 255], dtype=np.uint8)
-    redLower2 = np.array([170, 180, 50], dtype=np.uint8)
-    redUpper2 = np.array([180, 255, 255], dtype=np.uint8)
-    greenLower = np.array([70, 80, 40], dtype=np.uint8)
-    greenUpper = np.array([90, 255, 255], dtype=np.uint8)
+        redLower = np.array([0, 150, 70], dtype=np.uint8)
+        redUpper = np.array([10, 255, 255], dtype=np.uint8)
+        redLower2 = np.array([170, 180, 50], dtype=np.uint8)
+        redUpper2 = np.array([180, 255, 255], dtype=np.uint8)
+        greenLower = np.array([70, 80, 40], dtype=np.uint8)
+        greenUpper = np.array([90, 255, 255], dtype=np.uint8)
 
-    red_range_1 = [redLower, redUpper]
-    red_range_2 = [redLower2, redUpper2]
-    green_range = [greenLower, greenUpper]
+        
+        red_range_1 = [redLower, redUpper]
+        red_range_2 = [redLower2, redUpper2]
+        green_range = [greenLower, greenUpper]
 
-    # Threshold the HSV images to detect red and green colors
-    red_mask_right = None
-    green_mask_left = None
-    if hsv_right is not None:
-        red_mask_right_1 = cv.inRange(hsv_right, np.array(red_range_1[0]), np.array(red_range_1[1]))
-        red_mask_right_2 = cv.inRange(hsv_right, np.array(red_range_2[0]), np.array(red_range_2[1]))
-        red_mask_right = cv.bitwise_or(red_mask_right_1, red_mask_right_2)  # Combine both red ranges
-    
-    if hsv_left is not None:
-        green_mask_left = cv.inRange(hsv_left, np.array(green_range[0]), np.array(green_range[1]))
-
-    # Check if any pixels are detected for red or green color in the right or left regions
-    if red_mask_right is not None and np.count_nonzero(red_mask_right) > 0:
-        color = -90 # Red
-    elif green_mask_left is not None and np.count_nonzero(green_mask_left) > 0:
-        color = 90 # Green
-    else:
-        color = -50 # White
+        # Threshold the HSV images to detect red and green colors
+        red_mask_right = None
+        green_mask_left = None
+        if hsv_right is not None:
+            red_mask_right_1 = cv.inRange(hsv_right, np.array(red_range_1[0]), np.array(red_range_1[1]))
+            red_mask_right_2 = cv.inRange(hsv_right, np.array(red_range_2[0]), np.array(red_range_2[1]))
+            red_mask_right = cv.bitwise_or(red_mask_right_1, red_mask_right_2)  # Combine both red ranges
+        
+        if hsv_left is not None:
+            green_mask_left = cv.inRange(hsv_left, np.array(green_range[0]), np.array(green_range[1]))
+        # Check if any pixels are detected for red or green color in the right or left regions
+        if red_mask_right is not None and np.count_nonzero(red_mask_right) > 0:
+            color = -90 #Red
+##            print("-90.0 / Red")
+        elif green_mask_left is not None and np.count_nonzero(green_mask_left) > 0:
+            color = 90 #Green
+##            print("90.0 / Green")
+        else:
+            color = -50 # Whtie
+##    cropped_frame = frame[roi_y_min:roi_y_max, roi_x_min_left:roi_x_max_right]
+##    cv.imshow("Aruco Detection", cropped_frame)
+##    print(color)
     return color
 
 # Run until the user types 'q' to quit
@@ -165,49 +178,64 @@ while True:
     
     # Convert to grayscale
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    
+##    cv.imshow("Image", frame)
     # Detect Aruco markers
     corners, ids, rejected = cv.aruco.detectMarkers(gray, dictionary, parameters=parameters)
 
     currDistance = 99.9
     currAngle = 99.9
     if ids is not None: # if we find markers
-        # Estimate pose for all detected markers
+
         rvecs, tvecs, _ = cv.aruco.estimatePoseSingleMarkers(corners, 0.05, cameraMatrix, dist)
         
         # Calculate the distance for each marker (in feet)
-        distances = [float(round(tvec[0][2] * 3.28, 1)) for tvec in tvecs]
+        distances = [float(round(tvec[0][2] * 3.28084, 1)) for tvec in tvecs]
         
         # Find the index of the closest marker
         closest_idx = np.argmin(distances)
         
-        # Use only the closest marker
-        closest_corners = [corners[closest_idx]]  # Wrap in list to match expected format
-        closest_ids = np.array([[ids[closest_idx][0]]])  # Wrap in array to match expected format
-        currDistance = distances[closest_idx]
-
+        # Access the closest marker directly
+        closest_corner = corners[closest_idx]
+        # closest_id = ids[closest_idx][0]
+        # closest_distance = distances[closest_idx]
+        currDistance = get_distance(closest_corner) # find the distance, use to determine if we need to send an angle
         if currDistance > DETECTION_THRESH:
             currDistance = 99.9
             try:
+##                print([currDistance, currAngle])
                 i2cARD.write_i2c_block_data(ARD_ADDR, 0, [currDistance, currAngle])  
             except:
                 continue
-        
-        # Check the distance: detect color within COLOR_THRESH, angle outside 1 foot
+        # Check the distance only detect color within Color thresh foot, and detect angle outside 1 foot
         if currDistance <= COLOR_THRESH:  
-            currAngle = get_color(closest_corners, frame, closest_ids)
+            currAngle = get_color(closest_corner,frame,ids) # Set the angle to the detected color
         else:
-            currAngle = get_angle(closest_corners)
+            currAngle = get_angle(closest_corner) # find the angle
+##            currColor = 0 # unused input, set it to something so no errors
     
         message = [currDistance, currAngle] # compile the current Distance and angle into a message to send to the arduino
    
+##        if time() - last_update_time >= 0.5 and (currAngle != lastAngle or currDistance != lastDistance):
+##            if currAngle != lastAngle:
+##                lastAngle = currAngle # update the angle
+##            if currDistance != lastDistance:
+##                lastDistance = currDistance # update the distance
+##            if currColor != lastColor:
+##                lastColor = currColor # update the color
+
     else:
         currAngle = 99.9
         currDistance = 99.9
+##        if currDistance != lastDistance or currAngle != lastAngle:
+##            lastAngle = currAngle
+##            lastDistance = currDistance  
+            
+##        currColor = 1 # place holder value
         message = [currDistance, currAngle] # compile the current Distance and angle into a message to send to the arduino
 
     # Try to send data, if I2C error, continue to next iteration
     data = struct.pack('ff', message[0], message[1])
+    ## print(message)
     try:
         i2cARD.write_i2c_block_data(ARD_ADDR, 1, list(data))   
     except:
